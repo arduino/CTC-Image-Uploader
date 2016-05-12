7daec9f1dca7767b1ed5870a19080f0af7a60f31
X-Y <x.yang@arduino.cc>
2016-05-12 11:15:00 +0200

    Started working on angularJS frontend

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from jinja2 import Environment
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
+
 
 from mainDB import CTCPhotoDB
 from flickr_procedure import getFilenameFromRec
 
-app = Flask(__name__,static_folder='data')
+app = Flask(__name__,static_folder="frontend")
 app.debug=True
 
 @app.template_filter("imgPath")
@@ -12,6 +17,11 @@ def getImgPath(rec):
 	global db
 	return getFilenameFromRec(rec,db)
 
+'''
+@app.route('/<path:filename>')
+def base_static(filename):
+    return send_from_directory(app.root_path + '/data/', filename)
+'''
 @app.route('/')
 def hello_world():
 	global db
@@ -24,6 +34,12 @@ def hello_world():
 		records.append(record)
 	return render_template("testMainTemplate.html",records=records)
 
+'''
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",threaded=True)
+'''
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0")
+  http_server = HTTPServer(WSGIContainer(app))
+  http_server.listen(5000)
+  IOLoop.instance().start()
