c512883975545b4cddf4b0e2057a24569175aa2a
X-Y <x.yang@arduino.cc>
2016-05-12 17:07:22 +0200

    Adding REST apis

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 from jinja2 import Environment
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-
+import json
 
 
 from mainDB import CTCPhotoDB
@@ -11,6 +11,45 @@ from flickr_procedure import getFilenameFromRec
 
 app = Flask(__name__,static_url_path="", static_folder="frontend/app")
 app.debug=True
+
+def convertToJson(rows):
+	return json.dumps([dict(one) for one in rows])
+
+@app.route("/photosets/all",methods=["GET"])
+def getAllPhotoSets():
+	global db
+	db=CTCPhotoDB()
+	photoSets=db.getAllSets()
+	#res=convertToJson(photoSets)
+	res=[]
+	for one in photoSets:
+		res.append({	\
+			"title":one["name"],	\
+			"set_id":one["set_id"],	\
+			"hosted_id":one["hosted_id"],	\
+			"state":one["state"]
+			})
+
+	return jsonify({"photosets":res})
+
+@app.route("/photosets/<int:set_id>",methods=["GET"])
+def getPhotosInSet(set_id):
+	global db
+	db=CTCPhotoDB()
+	photos=db.getPhotosBySetID(set_id)
+	#res=convertToJson(photos)
+	res=[]
+	for one in photos:
+		res.append({	\
+			"title":one["file_name"],	\
+			"order":one["order_in_set"], \
+			"local_src":getFilenameFromRec(one,db),	\
+			"hosted_src":one["hosted_url"],	\
+			"synced":one["synced"]
+			})
+
+	return jsonify({"photos":res})
+
 
 @app.template_filter("imgPath")
 def getImgPath(rec):
